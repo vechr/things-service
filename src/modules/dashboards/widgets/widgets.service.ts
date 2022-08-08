a744da8a7320114ee55fd2d49619ac1b8620c8e6
zulfikar4568 <isnaen70@gmail.com>
2022-08-09 01:43:47 +0700

    :zap: widgets service

--- src/modules/dashboards/widgets/widgets.service.ts
+++ src/modules/dashboards/widgets/widgets.service.ts
@@ -1,7 +1,12 @@
 import PrismaService from '@/prisma/prisma.service';
-import { NotFoundException } from '@/shared/exceptions/common.exception';
+import {
+  NotFoundException,
+  UnknownException,
+} from '@/shared/exceptions/common.exception';
+import log from '@/shared/utils/log.util';
 import { HttpStatus, Injectable } from '@nestjs/common';
 import { Widget } from '@prisma/client';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import { CreateWidgetDto } from './dto/create-widget.dto';
 
 @Injectable()
@@ -9,17 +14,29 @@ export class WidgetService {
   constructor(private readonly prisma: PrismaService) {}
 
   async getWidgets(dashboardId: string): Promise<Widget[]> {
-    const result = await this.prisma.widget.findMany({
-      where: {
-        dashboardId: dashboardId,
-      },
-      include: {
-        Dashboard: true,
-        topic: true,
-      },
-    });
+    try {
+      const result = await this.prisma.widget.findMany({
+        where: {
+          dashboardId: dashboardId,
+        },
+        include: {
+          Dashboard: true,
+          topic: true,
+        },
+      });
 
-    return result;
+      return result;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        log.error(error.message);
+        throw new UnknownException({
+          code: HttpStatus.INTERNAL_SERVER_ERROR.toString(),
+          message: `Error unexpected!`,
+          params: { exception: error.message },
+        });
+      }
+      throw error;
+    }
   }
 
   async createWidget(
@@ -36,70 +53,82 @@ export class WidgetService {
       widgetData,
     }: CreateWidgetDto,
   ): Promise<Widget> {
-    const checkDashboard = await this.prisma.dashboard.findUnique({
-      where: {
-        id: dashboardId,
-      },
-    });
-
-    if (!checkDashboard) {
-      throw new NotFoundException({
-        code: HttpStatus.NOT_FOUND.toString(),
-        message: 'Dashboard is not found!',
+    try {
+      const checkDashboard = await this.prisma.dashboard.findUnique({
+        where: {
+          id: dashboardId,
+        },
       });
-    }
 
-    const checkTopic = await this.prisma.topic.findUnique({
-      where: {
-        id: topicId,
-      },
-    });
+      if (!checkDashboard) {
+        throw new NotFoundException({
+          code: HttpStatus.NOT_FOUND.toString(),
+          message: 'Dashboard is not found!',
+        });
+      }
 
-    if (!checkTopic) {
-      throw new NotFoundException({
-        code: HttpStatus.NOT_FOUND.toString(),
-        message: 'Topic is not found!',
+      const checkTopic = await this.prisma.topic.findUnique({
+        where: {
+          id: topicId,
+        },
       });
-    }
 
-    const checkTopicDashboard = await this.prisma.topic.findMany({
-      where: {
-        device: {
-          dashboards: {
-            some: {
-              dashboardId: dashboardId,
+      if (!checkTopic) {
+        throw new NotFoundException({
+          code: HttpStatus.NOT_FOUND.toString(),
+          message: 'Topic is not found!',
+        });
+      }
+
+      const checkTopicDashboard = await this.prisma.topic.findMany({
+        where: {
+          device: {
+            dashboards: {
+              some: {
+                dashboardId: dashboardId,
+              },
             },
           },
         },
-      },
-    });
-
-    if (!checkTopicDashboard) {
-      throw new NotFoundException({
-        code: HttpStatus.NOT_FOUND.toString(),
-        message: 'Topic is belong to the Dashboard!',
       });
+
+      if (!checkTopicDashboard) {
+        throw new NotFoundException({
+          code: HttpStatus.NOT_FOUND.toString(),
+          message: 'Topic is belong to the Dashboard!',
+        });
+      }
+
+      const widget = await this.prisma.widget.create({
+        data: {
+          name,
+          description,
+          node,
+          nodeId,
+          widgetType,
+          widgetData,
+          persistance,
+          hidden,
+          dashboardId,
+          topicId,
+        },
+        include: {
+          Dashboard: true,
+          topic: true,
+        },
+      });
+
+      return widget;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        log.error(error.message);
+        throw new UnknownException({
+          code: HttpStatus.INTERNAL_SERVER_ERROR.toString(),
+          message: `Error unexpected!`,
+          params: { exception: error.message },
+        });
+      }
+      throw error;
     }
-
-    const widget = await this.prisma.widget.create({
-      data: {
-        name,
-        description,
-        node,
-        nodeId,
-        widgetType,
-        widgetData,
-        persistance,
-        hidden,
-        dashboardId,
-        topicId,
-      },
-      include: {
-        Dashboard: true,
-        topic: true,
-      },
-    });
-
-    return widget;
   }
 }
