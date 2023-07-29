e4ae6430a948f9a99d2ded9c42efd29e9b8979cf
zulfikar4568 <isnaen70@gmail.com>
2023-07-29 17:32:24 +0700

    feat: add instrumentation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
+import { OpenTelemetryModule } from 'nestjs-otel';
 import { LoggerModule } from 'nestjs-pino';
 import AuthModule from './core/auth.module';
 import AuditModule from './modules/audits/audit.module';
@@ -8,13 +9,40 @@ import { NatsjsModule } from './modules/services/natsjs/natsjs.module';
 import { TopicEventModule } from './modules/topic-events/topic-event.module';
 import { TopicModule } from './modules/topics/topic.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { InstrumentMiddleware } from './shared/middlewares/instrument.middleware';
 import { logger } from './shared/utils/log.util';
+
+const OpenTelemetryModuleConfig = OpenTelemetryModule.forRoot({
+  metrics: {
+    hostMetrics: true,
+    apiMetrics: {
+      enable: true,
+    },
+  },
+});
+
+const PinoLoggerModule = LoggerModule.forRoot({
+  pinoHttp: {
+    customLogLevel: function (_, res, err) {
+      if (res.statusCode >= 400 && res.statusCode < 500) {
+        return 'error';
+      } else if (res.statusCode >= 500 || err) {
+        return 'fatal';
+      } else if (res.statusCode >= 300 && res.statusCode < 400) {
+        return 'warn';
+      } else if (res.statusCode >= 200 && res.statusCode < 300) {
+        return 'info';
+      }
+      return 'debug';
+    },
+    logger,
+  },
+});
 
 @Module({
   imports: [
-    LoggerModule.forRoot({
-      pinoHttp: { logger },
-    }),
+    OpenTelemetryModuleConfig,
+    PinoLoggerModule,
     //plugins
     PrismaModule,
     NatsjsModule,
@@ -27,4 +55,10 @@ import { logger } from './shared/utils/log.util';
     TopicEventModule,
   ],
 })
-export class HttpModule {}
+export class HttpModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer
+      .apply(InstrumentMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
