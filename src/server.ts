7fccd13c64febbc431b5303f9b812a8a42fc803b
zulfikar4568 <isnaen70@gmail.com>
2023-08-13 20:47:19 +0700

    fix: / path health check

--- src/server.ts
+++ src/server.ts
@@ -36,7 +36,7 @@ const httpServer = new Promise(async (resolve, reject) => {
       );
 
     // Set prefix api globally
-    app.setGlobalPrefix('api', { exclude: ['health'] });
+    app.setGlobalPrefix('api', { exclude: ['health', '/'] });
 
     // Enable CORS for security
     app.enableCors({
